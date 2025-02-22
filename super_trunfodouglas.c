e86931bc51682d7e33af9d085268586917699e68
Douglasssbd <douglassbd@gmail.com>
2025-02-22 20:32:25 -0300

    Create super_trunfodouglas.c

---
+++ super_trunfodouglas.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+int main (){
+    char carta, estado[100];
+    char codigo[100];
+    char cidade[100];
+    float populacao;
+    int turisticos;
+    float area;
+    float pib;
+
+    printf("Carta: \n");
+    scanf("%d", &carta);
+
+    printf("Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Populacao: \n");
+    scanf("%f", &populacao);
+
+    printf("Area: \n");
+    scanf("%f", &area);
+
+    printf("PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Numero de Pontos Turisticos: \n");
+    scanf("%d", &turisticos);
+
+    printf("Carta: %d\n", carta);
+    printf("Estado: %s\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Populacao: %f\n", populacao);
+    printf("Area: %f KM2\n", area);
+    printf("PIB: %f Bilhoes de reais\n", pib);
+    printf("Numero de Pontos Turisticos: %d\n", turisticos);
+
+    
+
+    printf("Carta: \n");
+    scanf("%d", &carta);
+
+    printf("Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Populacao: \n");
+    scanf("%f", &populacao);
+
+    printf("Area: \n");
+    scanf("%f", &area);
+
+    printf("PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Numero de Pontos Turisticos: \n");
+    scanf("%d", &turisticos);
+    
+    printf("Carta: %d\n", carta);
+    printf("Estado: %s\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Populacao: %f\n", populacao);
+    printf("Area: %f KM2\n", area);
+    printf("PIB: %f Bilhoes de reais\n", pib);
+    printf("Numero de Pontos Turisticos: %d\n", turisticos);
+    
+
+    return 0;
+
+    
+
+ }
